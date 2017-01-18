6d5a2d0980fbefced4f86e9129f71c1fe38a2b71
rod <rod@DELL-ROD>
2017-01-18 20:37:28 +0800

    Add User page and Cleaned some codes

---
+++ ProjectSS.Web/Controllers/Admin/User.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.ApplicationInsights;
+using ProjectSS.Db.Contracts;
+using System.Web.Mvc;
+
+namespace ProjectSS.Web.Controllers.Admin
+{
+    [Authorize(Roles = "OM")]
+    public class User : BaseController
+    {
+        public User(IMapper mapper, IDataRepo repo, TelemetryClient telemetryClient)
+            : base(repo, mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        public ActionResult Index()
+        {
+
+            return View()
+        };
+    }
+}
