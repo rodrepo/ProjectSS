ea4f53289d35ecfdb9f6657c78977bf6ba9eb672
rod <rod@DELL-ROD>
2017-01-26 18:48:43 +0800

    Adds ViewModel and Mapping

---
+++ ProjectSS.Web/Controllers/Admin/ProposalController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSS.Web.Controllers.Admin
+{
+    public class ProposalController
+    {
+      
+    }
+}
