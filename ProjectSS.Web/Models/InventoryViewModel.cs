876c04ae648815decc27ddb7bf389e36d45d91a3
rod <rod@DELL-ROD>
2017-02-06 19:19:21 +0800

    Adds InventoryViewModel and Mapping

---
+++ ProjectSS.Web/Models/InventoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSS.Web.Models
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string SerialNo { get; set; }
+        public string Location { get; set; }
+        public string UserId { get; set; }
+        public int Quantity { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
